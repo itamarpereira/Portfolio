90af769ad6ddc62c861a5b7fca588473c06b8c1c
Itamar Pereira <itamartpereira@hotmail.com>
2020-06-25 13:36:13 -0300

    Create application helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def project_image_url(project)
-    project.photo.attached? ? "#{cl_image_path(project.photo.key)}" : "https://images.unsplash.com/photo-1557318041-1ce374d55ebf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+    project.photo.attached? ? "#{cl_image_path(project.photo.key)}" : "https://images.unsplash.com/photo-1554290712-e640351074bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=665&q=80"
   end
 end
