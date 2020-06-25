58388908ddd6f2c021cc74fd08dbb8a1b20c6a74
Itamar Pereira <itamartpereira@hotmail.com>
2020-06-24 23:20:02 -0300

    Create helper to add photos to project cards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def project_image_url(project)
+    project.photo.attached? ? "#{cl_image_path(project.photo.key)}" : "https://images.unsplash.com/photo-1557318041-1ce374d55ebf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+  end
 end
