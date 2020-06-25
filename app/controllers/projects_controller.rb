9a5c0096f1f870c0d0a86835b89296c54480cfef
Itamar Pereira <itamartpereira@hotmail.com>
2020-06-25 13:35:29 -0300

    Add strong params to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,14 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description, :url, :photo)
+  end
 end
