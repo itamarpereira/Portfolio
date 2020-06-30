0ad7f6d9fe083b6a178efc8c51efa8e9ccf5186a
Itamar Pereira <itamartpereira@hotmail.com>
2020-06-30 13:52:17 -0300

    Add port route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'projects#index'
-  resources :projects, only: [:index, :port]
+  resources :projects, only: [:index]
+  get '/port', to: 'projects#port'
 end
