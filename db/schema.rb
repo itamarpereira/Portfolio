531e10faf6c9f9fe5662d5996cd7f9c1e8f7abb6
Itamar Pereira <itamartpereira@hotmail.com>
2020-07-02 10:04:08 -0300

    Migrate new changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_091023) do
+ActiveRecord::Schema.define(version: 2020_07_02_125122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2020_07_02_091023) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "url"
     t.string "port_description"
+    t.bigint "selection_id", null: false
+    t.index ["selection_id"], name: "index_projects_on_selection_id"
   end
 
   create_table "selections", force: :cascade do |t|
@@ -55,7 +57,11 @@ ActiveRecord::Schema.define(version: 2020_07_02_091023) do
     t.string "theme"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "selection_id", null: false
+    t.index ["selection_id"], name: "index_visualizations_on_selection_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "projects", "selections"
+  add_foreign_key "visualizations", "selections"
 end
