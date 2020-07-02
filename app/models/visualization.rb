607375910eb2f9578d5a8f421339a90d12dd6474
Itamar Pereira <itamartpereira@hotmail.com>
2020-07-02 10:05:27 -0300

    Update visualization model

--- app/models/visualization.rb
+++ app/models/visualization.rb
@@ -1,2 +1,8 @@
 class Visualization < ApplicationRecord
+  LANGUAGES = ["english", "portuguese", "french"]
+  THEMES = ["solar","lunar"]
+
+  belongs_to :selection
+  validates :language, inclusion: { in: LANGUAGES, message: "%{value} is not a valid language" }
+  validates :theme, inclusion: { in: THEMES, message: "%{value} is not a valid theme" }
 end
